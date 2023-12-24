c0907a0ab988b0f6ddacd6f8e5dbf268e05a8b25
ngonguyen <tanngontn@gmail.com>
2023-12-24 17:19:56 -0600

    add middleware

---
+++ helper/jwt.go
@@ -0,0 +1,60 @@
+package helper
+
+import (
+	"diaryapi/model"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+func ValidateJWT(context *gin.Context) error {
+	token, err := getToken(context)
+	if err != nil {
+		return err
+	}
+	_, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		return nil
+	}
+	return errors.New("invalid token provided")
+}
+
+func CurrentUser(context *gin.Context) (model.User, error) {
+	err := ValidateJWT(context)
+	if err != nil {
+		return model.User{}, err
+	}
+	token, _ := getToken(context)
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userId := uint(claims["id"].(float64))
+
+	user, err := model.FindUserById(userId)
+	if err != nil {
+		return model.User{}, err
+	}
+	return user, nil
+}
+
+func getToken(context *gin.Context) (*jwt.Token, error) {
+	tokenString := getTokenFromRequest(context)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return privateKey, nil
+	})
+	return token, err
+}
+
+func getTokenFromRequest(context *gin.Context) string {
+	bearerToken := context.Request.Header.Get("Authorization")
+	splitToken := strings.Split(bearerToken, " ")
+	if len(splitToken) == 2 {
+		return splitToken[1]
+	}
+	return ""
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"diaryapi/controller"
 	"diaryapi/database"
+	"diaryapi/middleware"
 	"diaryapi/model"
 	"fmt"
 	"log"
@@ -36,6 +37,11 @@ func serveApplication() {
 	publicRoutes := router.Group("/auth")
 	publicRoutes.POST("/register", controller.Register)
 	publicRoutes.POST("/login", controller.Login)
+
+	protectedRoutes := router.Group("/api")
+	protectedRoutes.Use(middleware.JWTAuthMiddleware())
+	protectedRoutes.POST("/entry", controller.AddEntry)
+	protectedRoutes.GET("/entry", controller.GetAllEntries)
 
 	router.Run(":8000")
 	fmt.Println("Server running on port 8000")
